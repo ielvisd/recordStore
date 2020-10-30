64da2f86745ac0ebf08172a4221f2d17efaa48fe
elvis-narrate <elvis@narrateapp.com>
2020-10-29 18:07:35 -0700

    Update routes, index & fix types. server boots, no data yet

--- app/controllers/api/v1/records_controller.rb
+++ app/controllers/api/v1/records_controller.rb
@@ -1,11 +1,14 @@
 module Api
   module V1
     class RecordsController < ApplicationController
+      # Sign in before being able to access
+      before_action :authorize_access_request!
       before_action :set_record, only: [:show, :update, :destroy]
 
       # GET /records
       def index
-        @records = Record.all
+        # scope to current user! DO FOR ARTIST
+        @records = current_user.records.all
 
         render json: @records
       end
@@ -17,7 +20,7 @@ module Api
 
       # POST /records
       def create
-        @record = Record.new(record_params)
+        @record = current_user_records.build(record_params)
 
         if @record.save
           render json: @record, status: :created, location: @record
@@ -43,12 +46,12 @@ module Api
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_record
-          @record = Record.find(params[:id])
+          @record = curent_user_records.find(params[:id])
         end
 
         # Only allow a trusted parameter "white list" through.
         def record_params
-          params.require(:record).permit(:title, :year, :artist_id, :users_id)
+          params.require(:record).permit(:title, :year, :artist_id)
         end
     end
   end
